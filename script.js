96354374214aeb72fb4e5ce725b4f60508b8f3b4
kindaserene <thanhbinhvu.q@gmail.com>
2025-08-17 17:01:01 +0700

    release first workable of calculator, only support virtual keys input

--- script.js
+++ script.js
@@ -4,9 +4,19 @@ const display = document.getElementById("display");
 let num1 = null;
 let operator = null;
 let num2 = null;
+let waitNum2 = false;
 
 function appendToDisplay(input) {
-    display.value += input;
+    if (input === '.' && display.value.includes('.')) {
+        return;
+    }
+    if (waitNum2 === false) {
+        display.value += input;
+    } else {
+        clearDisplay();
+        waitNum2 = false;
+        display.value += input;                
+    }
 };
 
 function add(a, b) {
@@ -23,20 +33,41 @@ function multiply(a, b) {
 
 function divide(a, b) {
     return a / b;
-};
+};    
 
-function calculate(a, op, b) {
-    if (op === '+' ) {
-        return add(a, b);
-    } else if (op === '-' ) {
-        return subtract(a, b);
-    } else if (op === '*') {
-        return multiply(a, b);
-    } else {
-        return divide(a, b);
+function calculate() {
+    if (num1 !== null && operator !== null && display.value !== "") {
+        num2 = Number(display.value);
+        let res;
+
+        if (operator === '+' ) {
+            res = add(num1, num2);
+        } else if (operator === '-' ) {
+            res = subtract(num1, num2);
+        } else if (operator === '*') {
+            res = multiply(num1, num2);
+        } else {
+            res = divide(num1, num2);
+        }
+
+        display.value = res;
+
+        num1 = null;
+        num2 = null;
+        operator = null;
+        waitNum2 = false;                
+    } else if (num1 === null && operator === null) {
+        display.value = "";
     }
 };
 
 function clearDisplay() {
     display.value = "";
 };
+
+function handleOperator(op) {
+    num1 = Number(display.value);
+    operator = op;
+    waitNum2 = true;
+}
+
