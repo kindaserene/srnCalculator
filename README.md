57e9bbf75e68bec1069137412cf4326f04fcfaf1
kindaserene <thanhbinhvu.q@gmail.com>
2025-08-17 16:20:45 +0700

    update calculation script

--- README.md
+++ README.md
@@ -1,1 +1,6 @@
 # Srn's Calculator
+
+#2176cc
+#ff7d6e
+#fca6ac
+#e8e7cb
--- script.js
+++ script.js
@@ -1,13 +1,42 @@
+console.log("Script is working!");
+
 const display = document.getElementById("display");
+let num1 = null;
+let operator = null;
+let num2 = null;
 
 function appendToDisplay(input) {
-    
+    display.value += input;
 };
 
-function calculate() {
-
+function add(a, b) {
+    return a + b;
 };
 
-function clear() {
+function subtract(a, b) {
+    return a - b;
+};
 
+function multiply(a, b) {
+    return a * b;
+};
+
+function divide(a, b) {
+    return a / b;
+};
+
+function calculate(a, op, b) {
+    if (op === '+' ) {
+        return add(a, b);
+    } else if (op === '-' ) {
+        return subtract(a, b);
+    } else if (op === '*') {
+        return multiply(a, b);
+    } else {
+        return divide(a, b);
+    }
+};
+
+function clearDisplay() {
+    display.value = "";
 };
